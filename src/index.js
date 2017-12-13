b5f5fa06f52d20eba4586755aaab42e77baa893d
Aleksander Repin <alex.repin@mail.ru>
2017-12-13 14:23:44 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,333 @@
+/*
+	:property.sub1.func().funcProp
+	 = return funcProp of function result of sub1 property of property of object
+	:{::helperVal}.sub
+	 = return sub property of object property with name retrieved from helperVal property of helpers object
+	:{::helperFunc()}.sub
+	= return sub property of object property with name retrieved from helperVal function result of helpers object.
+	if helpersFunx return 'car' then source path becomes :car.sub
+
+*/
+
+const SUB_PATH_START = '{',
+	SUB_PATH_END = '}',
+	PATH_SPLIT = '.',
+	PATH_START_OBJECT = ':',
+	PATH_START_HELPERS = '::',
+	FUNCTION_MARKER = '()',
+	MAX_DEEP = 10;
+
+/**
+ * Set of tools to use notPath property access notation
+ * : is for item
+ * :: is for helpers
+ * {} subpath
+ * . path splitter
+ * () function and should be executed with params (item, helper | undefined)
+ * sub-paths will be parsed and replaced by results in source path
+ */
+class notPath {
+	constructor() {
+		return this;
+	}
+	/*
+		input ':{::helperVal}.sub'
+		return ::helperVal
+	*/
+
+	/**
+	 * Returns first subpath in path
+	 * if subpath not closed will return it anyway
+	 * @param {string} path path in string notation
+	 * @return {string|null} subpath or null if no sub path were found
+	 */
+	findNextSubPath(path) {
+		let subPath = '',
+			find = false;
+		for (let i = 0; i < path.length; i++) {
+			if (path[i] === SUB_PATH_START) {
+				find = true;
+				subPath = '';
+			} else {
+				if ((path[i] === SUB_PATH_END) && find) {
+					return subPath;
+				} else {
+					subPath += path[i];
+				}
+			}
+		}
+		return find ? subPath : null;
+	}
+
+	/**
+	 * Replace sub-path in parent path by parsed version
+	 * @param {string} path path to process
+	 * @param {string} sub sub path to replace
+	 * @param {string} parsed parsed sub path
+	 * @return {string} parsed path
+	 */
+
+	replaceSubPath(path, sub, parsed) {
+		let subf = SUB_PATH_START + sub + SUB_PATH_END,
+			i = 0;
+		while ((path.indexOf(subf) > -1) && i < MAX_DEEP) {
+			path = path.replace(subf, parsed);
+			i++;
+		}
+		return path;
+	}
+
+	/**
+	 * Parses path while there any sub-paths
+	 * @param {string} path raw unparsed path
+	 * @param {object} item data
+	 * @param {object} helpers helpers
+	 * @return {string} parsed path
+	 */
+	parseSubs(path, item, helpers) {
+		let subPath = this.findNextSubPath(path),
+			subPathParsed, i = 0;
+		while (subPath) {
+			subPathParsed = this.getValueByPath(subPath.indexOf(PATH_START_HELPERS) > -1 ? helpers : item, subPath, item, helpers);
+			path = this.replaceSubPath(path, subPath, subPathParsed);
+			i++;
+			if (i > MAX_DEEP) {
+				break;
+			}
+			subPath = this.findNextSubPath(path);
+		}
+		return path;
+	}
+
+	/**
+	 * Get property value
+	 * @param {string} path path to property
+	 * @param {object} item item object
+	 * @param {object} helpers helpers object
+	 */
+
+	get(path, item, helpers) {
+		switch (path) {
+		case PATH_START_OBJECT:
+			return item;
+		case PATH_START_HELPERS:
+			return helpers;
+		}
+		path = this.parseSubs(path, item, helpers);
+		return this.getValueByPath(path.indexOf(PATH_START_HELPERS) > -1 ? helpers : item, path, item, helpers);
+	}
+
+	/**
+	 * Set property value
+	 * @param {string} path path to property
+	 * @param {object} item item object
+	 * @param {object} helpers helpers object
+	 * @param {any} attrValue value we want to assign
+	 */
+
+	set(path, item, helpers, attrValue) {
+		if (arguments.length === 3) {
+			attrValue = helpers;
+			helpers = undefined;
+		}
+		let subPath = this.findNextSubPath(path),
+			subPathParsed,
+			i = 0;
+		while (subPath) {
+
+			subPathParsed = this.getValueByPath(subPath.indexOf(PATH_START_HELPERS) > -1 ? helpers : item, subPath, item, helpers);
+
+			path = this.replaceSubPath(path, subPath, subPathParsed);
+
+			if (i > MAX_DEEP) {
+				break;
+			}
+			subPath = this.findNextSubPath(path);
+			i++;
+		}
+
+		this.setValueByPath(item, path, attrValue);
+
+		if (item.isRecord && this.normilizePath(path).length > 1 && item.__isActive) {
+			item.trigger('change', item, path, attrValue);
+		}
+	}
+
+	/**
+	 * Set target property to null
+	 * @param {string} path path to property
+	 * @param {object} item item object
+	 * @param {object} helpers helpers object
+	 */
+
+	unset(path, item, helpers) {
+		this.set(path, item, helpers, null);
+	}
+
+	/**
+	 * Parses step key, transforms it to end-form
+	 * @param {string} step not parsed step key
+	 * @param {object} item item object
+	 * @param {object} helper helpers object
+	 * @return {string|number} parsed step key
+	 */
+
+	parsePathStep(step, item, helper) {
+		let rStep = null;
+		if (step.indexOf(PATH_START_HELPERS) === 0 && helper) {
+			rStep = step.replace(PATH_START_HELPERS, '');
+			if (rStep.indexOf(FUNCTION_MARKER) === rStep.length - 2) {
+				rStep = rStep.replace(FUNCTION_MARKER, '');
+				if (helper.hasOwnProperty(rStep)) {
+					return helper[rStep](item, undefined);
+				}
+			} else {
+				return helper[rStep];
+			}
+		} else {
+			if (step.indexOf(PATH_START_OBJECT) === 0 && item) {
+				rStep = step.replace(PATH_START_OBJECT, '');
+				if (rStep.indexOf(FUNCTION_MARKER) === rStep.length - 2) {
+					rStep = rStep.replace(FUNCTION_MARKER, '');
+					if (item.hasOwnProperty(rStep)) {
+						return item[rStep](item, undefined);
+					}
+				} else {
+					return item[rStep];
+				}
+			}
+		}
+		return step;
+	}
+
+	//::fieldName.result
+	//{}
+	//{fieldName: 'targetRecordField'}
+	////['targetRecordField', 'result']
+	/**
+	 * Transforms path with sub paths to path without
+	 * @param {string|array} path path to target property
+	 * @param {object} item item object
+	 * @param {object} helper helper object
+	 * @return {array} parsed path
+	 **/
+	parsePath(path, item, helper) {
+		if (!Array.isArray(path)) {
+			path = path.split(PATH_SPLIT);
+		}
+		for (var i = 0; i < path.length; i++) {
+			path[i] = this.parsePathStep(path[i], item, helper);
+		}
+		return path;
+	}
+
+	/**
+	 * Transforms path from string notation to array of keys
+	 * @param {string|array} path  input path, if array does nothing
+	 * @return {array} path in array notation
+	 */
+
+	normilizePath(path) {
+		if (Array.isArray(path)) {
+			return path;
+		} else {
+			while (path.indexOf(PATH_START_OBJECT) > -1) {
+				path = path.replace(PATH_START_OBJECT, '');
+			}
+			return path.split(PATH_SPLIT);
+		}
+	}
+
+	/*
+		small = ["todo"],
+		big = ["todo", "length"]
+		return true;
+
+	*/
+
+	/**
+	 * Identifies if first path includes second, compared from start,
+	 * no floating start position inside ['join', 'me'], ['me']
+	 * will result in false
+	 * @param {array} big where we will search
+	 * @param {array} small what we will search
+	 * @return {boolean} if we succeed
+	 */
+
+	ifFullSubPath(big, small) {
+		if (big.length < small.length) {
+			return false;
+		}
+		for (let t = 0; t < small.length; t++) {
+			if (small[t] !== big[t]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Getter through third object
+	 * Path is parsed, no event triggering for notRecord
+	 * @param {object} object object to be used as getter
+	 * @param {string|array} attrPath path to property
+	 * @param {object} item supporting data
+	 * @param {helpers} object  supporting helpers
+	 */
+
+	getValueByPath(object, attrPath, item, helpers) {
+		attrPath = this.normilizePath(attrPath);
+		let attrName = attrPath.shift(),
+			isFunction = attrName.indexOf(FUNCTION_MARKER) > -1;
+		if (isFunction) {
+			attrName = attrName.replace(FUNCTION_MARKER, '');
+		}
+		if ((typeof object === 'object' && typeof object !== 'undefined' && object!== null) && typeof object[attrName] !== 'undefined' && object[attrName] !== null) {
+			let newObj = isFunction ? object[attrName]({
+				item,
+				helpers
+			}) : object[attrName];
+			if (attrPath.length > 0) {
+				return this.getValueByPath(newObj, attrPath, item, helpers);
+			} else {
+				return newObj;
+			}
+		} else {
+			return undefined;
+		}
+	}
+
+	/**
+	 * Setter through third object
+	 * Path is parsed, no event triggering for notRecord
+	 * @param {object} object object to be modified
+	 * @param {string|array} attrPath path to property
+	 * @param {any} attrValue  value to assign
+	 */
+
+	setValueByPath(object, attrPath, attrValue) {
+		attrPath = this.normilizePath(attrPath);
+		let attrName = attrPath.shift();
+		if (attrPath.length > 0) {
+			if (!object.hasOwnProperty(attrName)) {
+				object[attrName] = {};
+			}
+			this.setValueByPath(object[attrName], attrPath, attrValue);
+		} else {
+			object[attrName] = attrValue;
+		}
+	}
+
+	/**
+	* Joins passed in strings with PATH_SPLIT
+	* @param {string} arguments path to be glued
+	* @return {string} composite path
+	*/
+
+	join() {
+		let args = Array.prototype.slice.call(arguments);
+		return args.join(PATH_SPLIT);
+	}
+}
+
+module.exports = new notPath();
---
+++ test/index.js
@@ -0,0 +1,372 @@
+const expect = require("chai").expect,
+	notPath = require('../src/index.js');
+
+describe("notPath", function() {
+	describe("finding sub path", function() {
+		it(":authors.{::index}", function() {
+			let path = ':authors.{::index}',
+				result = '::index';
+			expect(notPath.findNextSubPath(path)).to.equal(result);
+		});
+
+		it(":authors.{::index.{:version}}", function() {
+			let path = ':authors.{::index.{:version}}',
+				result = ':version';
+			expect(notPath.findNextSubPath(path)).to.equal(result);
+		});
+
+	});
+
+	describe("get", function() {
+		it(":", function() {
+			let path = ':',
+				item = {
+					authors: ['help', 'me']
+				},
+				helpers = {
+
+				};
+			expect(notPath.get(path, item, helpers)).to.equal(item);
+		});
+
+		it(":authors.{::obj.index}", function() {
+			let path = ':authors.{::obj.index}',
+				item = {
+					authors: ['help', 'me']
+				},
+				helpers = {
+					index: 0,
+					obj: {
+						index: 1
+					}
+				},
+				result = 'me';
+			expect(notPath.get(path, item, helpers)).to.equal(result);
+		});
+
+		it(":authors.{::index}", function() {
+			let path = ':authors.{::index}',
+				item = {
+					authors: ['help', 'me']
+				},
+				helpers = {
+					index: 0,
+					obj: {
+						index: 1
+					}
+				},
+				result = 'help';
+			expect(notPath.get(path, item, helpers)).to.equal(result);
+		});
+
+		it("::", function() {
+			let path = '::',
+				item = {
+					authors: ['help', 'me']
+				},
+				helpers = {
+					index: 0,
+					obj: {
+						index: 1
+					}
+				},
+				result = helpers;
+			expect(notPath.get(path, item, helpers)).to.equal(result);
+		});
+
+		it(":hi().title", function() {
+			let path = ':hi().title',
+				item = {
+					authors: ['help', 'me', 'awesome!'],
+					hi: function() {
+						return {
+							title: 'title',
+							index: 2
+						};
+					}
+				},
+				helpers = {},
+				result = 'title';
+			expect(notPath.get(path, item, helpers, result)).to.equal(result);
+		});
+
+		it(":authors.{:hi().index}", function() {
+			let path = ':authors.{:hi().index}',
+				item = {
+					authors: ['help', 'me', 'awesome!'],
+					hi: function() {
+						return {
+							index: 2
+						};
+					}
+				},
+				helpers = {},
+				result = 'awesome!';
+			expect(notPath.get(path, item, helpers, result)).to.equal(result);
+		});
+
+	});
+
+
+
+	describe("set", function() {
+
+		it(":authors without helpers", function() {
+			let path = ':authors',
+				item = {
+					authors: false
+				},
+				result = 'notMe';
+			notPath.set(path, item, result);
+			expect(item.authors).to.equal(result);
+		});
+
+		it("recursion in sub path", function() {
+			let path = ':authors.{::subpath}',
+				item = {
+					authors: {}
+				},
+				helpers = {
+					subpath: '{::subpath}'
+				},
+				result = 'notMe';
+			notPath.set(path, item, helpers, result);
+			expect(item.authors['{subpath}']).to.equal('notMe');
+		});
+
+		it(":authors.{::obj.index}", function() {
+			let path = ':authors.{::obj.index}',
+				item = {
+					authors: ['help', 'me']
+				},
+				helpers = {
+					index: 0,
+					obj: {
+						index: 1
+					}
+				},
+				result = 'notMe';
+			notPath.set(path, item, helpers, result);
+			expect(item.authors[1]).to.equal(result);
+		});
+
+		it(":authors.{::index}", function() {
+			let path = ':authors.{::index}',
+				item = {
+					authors: ['help', 'me']
+				},
+				helpers = {
+					index: 0,
+					obj: {
+						index: 1
+					}
+				},
+				result = 'not help';
+			notPath.set(path, item, helpers, result);
+			expect(item.authors[0]).to.equal(result);
+		});
+
+		it(":authors.{:hi().index}", function() {
+			let path = ':authors.{:hi().index}',
+				item = {
+					authors: ['help', 'me', 'awesome!'],
+					hi: function() {
+						return {
+							index: 2
+						};
+					}
+				},
+				helpers = {},
+				result = 'cool';
+			notPath.set(path, item, helpers, result);
+			expect(item.authors[2]).to.equal(result);
+		});
+
+		it(":authors.name when author not exists as object", function() {
+			let path = ':authors.name',
+				item = {
+					hi: function() {
+						return {
+							index: 2
+						};
+					}
+				},
+				helpers = {},
+				result = 'cool';
+			notPath.set(path, item, helpers, result);
+			expect(item.authors.name).to.equal(result);
+		});
+
+		it("trigger change", function() {
+			let path = ':authors.name',
+				result = false,
+				item = {
+					hi: function() {
+						return {
+							index: 2
+						};
+					},
+					__isActive: true,
+					isRecord: true,
+					trigger: ()=>{
+						result = true;
+					}
+				},
+				helpers = {};
+			notPath.set(path, item, helpers, result);
+			expect(result).to.be.ok;
+		});
+	});
+
+	describe("join", function() {
+		it("simple join path", function() {
+			let path1 = ':authors',
+				path2 = '0',
+				result = ':authors.0';
+			expect(notPath.join(path1, path2)).to.equal(result);
+		});
+
+		it("join two long paths", function() {
+			let path1 = ':authors.0',
+				path2 = 'aliases.2',
+				result = ':authors.0.aliases.2';
+			expect(notPath.join(path1, path2)).to.equal(result);
+		});
+	});
+
+	describe("findNextSubPath", function() {
+		it("not complete subPath, without closing", function() {
+			let path = 'join.{notClosedPath';
+			expect(notPath.findNextSubPath(path)).to.be.equal('notClosedPath');
+		});
+	});
+
+	describe("ifFullSubPath", function() {
+		it("join.army and join", function() {
+			let path1 = ['join', 'army'],
+				path2 = ['join'];
+			expect(notPath.ifFullSubPath(path1, path2)).to.be.true;
+		});
+
+		it("join.army and join.me.at.bar", function() {
+			let path1 = ['join', 'army'],
+				path2 = ['join','me','at','bar'];
+			expect(notPath.ifFullSubPath(path1, path2)).to.be.false;
+		});
+
+		it("join.army and join.me", function() {
+			let path1 = ['join', 'army'],
+				path2 = ['join','me'];
+			expect(notPath.ifFullSubPath(path1, path2)).to.be.false;
+		});
+	});
+
+	describe("parsePath", function() {
+		it("pass string join.army", function() {
+			let path1 = 'join.army',
+				path2 = ['join','army'];
+			expect(notPath.parsePath(path1)).to.be.deep.equal(path2);
+		});
+
+		it("pass array ['join','army']", function() {
+			let path1 = ['join','army'],
+				path2 = ['join','army'];
+			expect(notPath.parsePath(path1)).to.be.deep.equal(path2);
+		});
+	});
+
+	describe("parsePathStep", function() {
+		it(":property", function() {
+			let path = ':join',
+				item = {
+					join: 'subpath',
+					subpath: 'subpath_result'
+				},
+				helper = {},
+				result = 'subpath';
+			expect(notPath.parsePathStep(path, item, helper)).to.be.equal(result);
+		});
+
+		it(":function", function() {
+			let path = ':pol()',
+				item = {
+					pol: () => {return 'subpath';},
+					subpath: 'subpath_result'
+				},
+				helper = {},
+				result = 'subpath';
+			expect(notPath.parsePathStep(path, item, helper)).to.be.equal(result);
+		});
+
+		it(":function not exists", function() {
+			let path = ':polak()',
+				item = {
+					pol: () => {return 'subpath';},
+					subpath: 'subpath_result'
+				},
+				helper = {},
+				result = 'subpath';
+			expect(notPath.parsePathStep(path, item, helper)).to.be.equal(path);
+		});
+
+		it("::property", function() {
+			let path = '::join',
+				item = {
+
+				},
+				helper = {
+					join: 'subpath',
+					subpath: 'subpath_result'
+				},
+				result = 'subpath';
+			expect(notPath.parsePathStep(path, item, helper)).to.be.equal(result);
+		});
+
+		it("::function", function() {
+			let path = '::pol()',
+				item = {
+
+				},
+				helper = {
+					pol: () => {return 'subpath';},
+					subpath: 'subpath_result'
+				},
+				result = 'subpath';
+			expect(notPath.parsePathStep(path, item, helper)).to.be.equal(result);
+		});
+
+		it("::function not exists", function() {
+			let path = '::polak()',
+				item = {},
+				helper = {
+					pol: () => {return 'subpath';},
+					subpath: 'subpath_result'
+				},
+				result = 'subpath';
+			expect(notPath.parsePathStep(path, item, helper)).to.be.equal(path);
+		});
+	});
+
+	describe("getValueByPath", function() {
+		it("pass null as object", function() {
+			let path = ':name',
+				obj = null,
+				helpers = {},
+				result = 'awesome!';
+			expect(notPath.getValueByPath( obj, path, {}, helpers)).to.be.undefined;
+		});
+	});
+
+	describe("unset", function() {
+		it("pass string join.army", function() {
+			let item = {
+					join:{
+						army: true
+					}
+				},
+				path = 'join.army';
+			notPath.unset(path,item);
+			expect(item.join.army).to.be.not.ok;
+		});
+	});
+});
